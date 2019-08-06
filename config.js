1cd55f92dfccb86cc29e5d296af5f1fb5fb07e4f
n8tb1t <n8tb1t@gmail.com>
2019-08-06 17:58:34 +0500

    feat: deploy to zeit@now.

---
+++ config.js
@@ -0,0 +1,8 @@
+var api = 'https://cxex.catalystcrypto.net/cxex-explorer';
+// var api = 'http://cx.turbomine.co:17291';
+// var poolListUrl = '';
+var donationAddress = "";
+var blockTargetInterval = 60;
+var coinUnits = 10000000;
+var symbol = 'CX';
+var refreshDelay = 5000;
